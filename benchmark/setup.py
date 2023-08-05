1f8b8d6518b55eb6e1770b9313ec513ed329b1f5
shitao <stxiao@bupt.edu.cn>
2023-08-05 20:17:24 +0800

    update setup

--- benchmark/setup.py
+++ benchmark/setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", mode="r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='C_MTEB',
     version='1.0.0',
+    description='Chinese Massive Text Embedding Benchmark',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author_email='2906698981@qq.com',
     url='https://github.com/FlagOpen/FlagEmbedding/tree/master/benchmark',
     package_dir={"": "C_MTEB"},
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", mode="r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='flag_embedding',
-    version='1.0.0',
+    version='1.0.1',
+    description='FlagEmbedding',
+    long_description=readme,
+    long_description_content_type="text/markdown",
     author_email='2906698981@qq.com',
     url='https://github.com/FlagOpen/FlagEmbedding',
     package_dir={"": "flag_embedding"},
