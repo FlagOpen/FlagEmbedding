46f335b4a186966ee2d9283e5e19f9b8cbad8b3c
shitao <stxiao@bupt.edu.cn>
2023-08-05 22:53:05 +0800

    rename

--- C_MTEB/setup.py
+++ C_MTEB/setup.py
@@ -11,8 +11,7 @@ setup(
     long_description_content_type="text/markdown",
     author_email='2906698981@qq.com',
     url='https://github.com/FlagOpen/FlagEmbedding/tree/master/benchmark',
-    package_dir={"": "C_MTEB"},
-    packages=find_packages("C_MTEB"),
+    packages=find_packages(),
     install_requires=[
         'mteb[beir]',
     ],
--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ with open("README.md", mode="r", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='flag_embedding',
+    name='FlagEmbedding',
     version='1.0.1',
     description='FlagEmbedding',
     long_description=readme,
     long_description_content_type="text/markdown",
     author_email='2906698981@qq.com',
     url='https://github.com/FlagOpen/FlagEmbedding',
-    package_dir={"": "flag_embedding"},
-    packages=find_packages("flag_embedding"),
+    packages=find_packages(),
     install_requires=[
         'torch>=1.6.0',
         'transformers>=4.18.0',
