8f3657cda6fa9ddd897f0917c089defcba752d51
Niklas Muennighoff <n.muennighoff@gmail.com>
2023-09-14 19:22:05 +0200

    Fix name

--- C_MTEB/C_MTEB/__init__.py
+++ C_MTEB/C_MTEB/__init__.py
@@ -3,7 +3,7 @@ from .tasks import *
 ChineseTaskList = ['TNews', 'IFlyTek', 'MultilingualSentiment', 'JDReview', 'OnlineShopping', 'Waimai',
                    'CLSClusteringS2S', 'CLSClusteringP2P', 'ThuNewsClusteringS2S', 'ThuNewsClusteringP2P',
                    'Ocnli', 'Cmnli',
-                   'T2Reranking', 'MmarcoReranking', 'CMedQAv1', 'CMedQAv2',
+                   'T2Reranking', 'MMarcoReranking', 'CMedQAv1', 'CMedQAv2',
                    'T2Retrieval', 'MMarcoRetrieval', 'DuRetrieval', 'CovidRetrieval', 'CmedqaRetrieval', 'EcomRetrieval', 'MedicalRetrieval', 'VideoRetrieval',
                    'ATEC', 'BQ', 'LCQMC', 'PAWSX', 'STSB', 'AFQMC', 'QBQTC']
 
