89fdb823822c470adc7c7601c779adbcc1555b5a
JUNJIE99 <49506150+JUNJIE99@users.noreply.github.com>
2024-04-01 10:48:17 +0800

    Update modeling.py

--- FlagEmbedding/visual/modeling.py
+++ FlagEmbedding/visual/modeling.py
@@ -9,7 +9,7 @@ from transformers import AutoModel, AutoTokenizer, AutoConfig
 from transformers.file_utils import ModelOutput
 
 
-from eva_clip import create_eva_vision_and_transforms
+from FlagEmbedding.visual.eva_clip import create_eva_vision_and_transforms
 from PIL import Image
 
 logger = logging.getLogger(__name__)
