3bfd718372712a49aaac0a65084cbf67b6dd58c5
JUNJIE99 <49506150+JUNJIE99@users.noreply.github.com>
2024-04-01 10:50:28 +0800

    Update transformer.py

--- FlagEmbedding/visual/eva_clip/transformer.py
+++ FlagEmbedding/visual/eva_clip/transformer.py
@@ -31,7 +31,7 @@ try:
     import xformers.ops as xops
 except ImportError:
     xops = None
-    print("Please 'pip install xformers'")
+    # print("Please 'pip install xformers'")
 
 class LayerNormFp32(nn.LayerNorm):
     """Subclass torch's LayerNorm to handle fp16 (by casting to float32 and back)."""
