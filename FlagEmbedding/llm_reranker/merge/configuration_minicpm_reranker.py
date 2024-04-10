83ea58f234821d4520628ffed906d31c466e5b56
cfli <545999961@qq.com>
2024-04-10 06:56:31 +0000

    add merge file in llm_reranker

--- FlagEmbedding/llm_reranker/merge/configuration_minicpm_reranker.py
+++ FlagEmbedding/llm_reranker/merge/configuration_minicpm_reranker.py
@@ -22,7 +22,6 @@
 from transformers.configuration_utils import PretrainedConfig
 from transformers.utils import logging
 
-
 logger = logging.get_logger(__name__)
 
 MINICPM_PRETRAINED_CONFIG_ARCHIVE_MAP = {}
