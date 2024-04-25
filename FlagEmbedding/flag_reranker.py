1e4d428145c17669eca172ab39a4358e5335864d
cfli <545999961@qq.com>
2024-04-25 14:24:54 +0800

    load local layerwise-reranker

--- FlagEmbedding/flag_reranker.py
+++ FlagEmbedding/flag_reranker.py
@@ -408,6 +408,7 @@ class LayerWiseFlagLLMReranker:
         self.model = AutoModelForCausalLM.from_pretrained(model_name_or_path,
                                                           cache_dir=cache_dir,
                                                           trust_remote_code=True,
+                                                          local_files_only=True,
                                                           torch_dtype=torch.bfloat16 if use_bf16 else torch.float32)
 
         self.model_name_or_path = model_name_or_path
