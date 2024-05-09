ea4873db1a235792d7a861c800da09107ac8043a
leon.kepler <leon.kepler@bytedance.com>
2024-05-09 12:06:55 +0800

    new

--- reranker_pretrain.py
+++ reranker_pretrain.py
@@ -3,30 +3,11 @@ import contextlib
 import os
 import sys
 import torch
-
-def find_unused_port(start_port=37625):
-    """
-    寻找一个从start_port开始的未使用的端口。
-    """
-    for port in range(start_port, start_port + 20000):  # 检查1000个端口
-        with contextlib.suppress(Exception):
-            for i in range(10):
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                    sock.bind(('', port))
-                return port
-    return None  # 如果没有找到，返回None
-
 args = (" ").join(sys.argv[1:])
 
 script_name = "embedding_run"
 
 # 使用示例
-port = find_unused_port()
-if port:
-    print(f"Found an unused port: {port}")
-else:
-    print("No unused port found.")
-
 num_gpus = torch.cuda.device_count()
 
 os.system("cd /opt/tiger/FlagEmbedding")
@@ -41,7 +22,7 @@ if not os.path.exists("/opt/tiger/train_15neg"): os.system("cp -r /mnt/bn/data-t
 
 # 构建训练命令
 command = f"""
-torchrun --master-port={port} --nproc_per_node {num_gpus} /opt/tiger/FlagEmbedding/FlagEmbedding/reranker/{script_name}.py {args}
+torchrun --standalone --nnodes=1 --nproc_per_node {num_gpus} /opt/tiger/FlagEmbedding/FlagEmbedding/reranker/{script_name}.py {args}
 """
 
 # 执行命令
