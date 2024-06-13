6dc41b2455d1ecd6b4115c3ce4b43f9bac28478f
JUNJIE99 <49506150+JUNJIE99@users.noreply.github.com>
2024-06-13 16:15:04 +0800

    Update open_bench.py

--- MLVU/evaluation/generation_evaluation/open_bench.py
+++ MLVU/evaluation/generation_evaluation/open_bench.py
@@ -98,17 +98,12 @@ class MLVU(Dataset):
 def main():
 
     data_list = {
-        "count": ("4_count.json", f"/LVBench_all/video/count", "video"),
-        "ego": ("3_ego.json", f"/LVBench_all/video/ego", "video"),
-        "needle": ("2_needle.json", f"/LVBench_all/video/needle", "video"),
-        "order": ("5_order.json", f"/LVBench_all/video/order", "video"),
-        "plotQA": ("1_plotQA.json", f"/LVBench_all/video/plotQA", "video"),
-        "anomaly_reco": ("6_anomaly_reco.json", f"/LVBench_all/video/anomaly_reco", "video"),
-        "topic_reasoning": ("7_topic_reasoning.json", f"/LVBench_all/video/topic_reasoning", "video")
+    "subPlot": ("8_sub_scene.json", f"MLVU_all/video/subPlot", "video", False),
+    "summary": ("9_summary.json", f"MLVU_all/video/summary", "video", False)
     }
    
 
-    data_dir = f"/LVBench_all/json"
+    data_dir = f"MLVU_all/json"
    
 
     dataset = MLVU(data_dir, data_list)
