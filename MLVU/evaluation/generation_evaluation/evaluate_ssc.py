aea76af53a4f0107b08366745027257bc4a4602b
JUNJIE99 <49506150+JUNJIE99@users.noreply.github.com>
2024-06-13 16:04:40 +0800

    Update evaluate_ssc.py

--- MLVU/evaluation/generation_evaluation/evaluate_ssc.py
+++ MLVU/evaluation/generation_evaluation/evaluate_ssc.py
@@ -1,9 +1,5 @@
-## subPlot最终评测脚本
-
 import openai
 import os
-os.environ['https_proxy'] = "http://127.0.0.1:15777"
-os.environ['http_proxy'] = "http://127.0.0.1:15777"
 import argparse
 import json
 import ast
@@ -11,8 +7,8 @@ from multiprocessing.pool import Pool
 from tqdm import tqdm
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="question-answer-generation-using-gpt-4")
-    parser.add_argument("--pred_path", default="/evaluation_lvbench/qwen/16_subPlot_all.json", help="The path to file containing prediction.")
+    parser = argparse.ArgumentParser(description="ssc-evaluation-gpt-4")
+    parser.add_argument("--pred_path", default="output_dir/qwen/16_subPlot_all.json", help="The path to file containing prediction.")
     parser.add_argument("--output_dir", default="output_dir/qwen_subplot_all", help="The path to save annotation json files.")
     parser.add_argument("--output_json", default="output_dir/qwen_subplot_all_results.json", help="The path to save annotation final combined json file.")
     parser.add_argument("--api_key", default="", help="OpenAI API key.")
@@ -21,7 +17,7 @@ def parse_args():
     return args
 
 
-def get_scoring_points(score_points="/LVBench_all/new_json/subPlot_scorepoint.json"):
+def get_scoring_points(score_points="/MLVU_all/new_json/subPlot_scorepoint.json"):
     q_s_dict = {}
     all_data = json.load(open(score_points, "r"))
     for data in all_data:
@@ -33,7 +29,7 @@ def get_scoring_points(score_points="/LVBench_all/new_json/subPlot_scorepoint.js
 
 def annotate(prediction_set, caption_files, output_dir):
     """
-    Evaluates question and answer pairs using GPT-3
+    Evaluates question and answer pairs using GPT-4
     Returns a score for correctness.
     """
     q_s_dict = get_scoring_points()
