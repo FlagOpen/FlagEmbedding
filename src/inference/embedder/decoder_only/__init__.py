7b3dc99b0fd0804cd76c9c17b8ca480406934911
hanhainebula <2512674094@qq.com>
2024-09-19 19:19:38 +0800

    upload inference code: LLM-based models

--- src/inference/embedder/decoder_only/__init__.py
+++ src/inference/embedder/decoder_only/__init__.py
@@ -1,2 +1,7 @@
-from src.inference.embedder.decoder_only._base import *
-from src.inference.embedder.decoder_only.icl import *
+from ._base import BaseLLMEmbedder as FlagLLMModel
+from .icl import ICLLLMEmbedder as FlagICLModel
+
+__all__ = [
+    "FlagLLMModel",
+    "FlagICLModel",
+]
