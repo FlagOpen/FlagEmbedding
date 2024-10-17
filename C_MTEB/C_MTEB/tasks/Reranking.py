2bfc92267c2efd0c943187cd459b23d22ba4c650
ZiyiXia <joeyxia0805@163.com>
2024-10-17 12:06:46 +0800

    update C-MTEB matcing MTEB new structure

--- C_MTEB/C_MTEB/tasks/Reranking.py
+++ C_MTEB/C_MTEB/tasks/Reranking.py
@@ -1,236 +1,152 @@
-import logging
+from __future__ import annotations
 
-import numpy as np
-from mteb import RerankingEvaluator, AbsTaskReranking
-from tqdm import tqdm
-
-logger = logging.getLogger(__name__)
-
-
-class ChineseRerankingEvaluator(RerankingEvaluator):
-    """
-    This class evaluates a SentenceTransformer model for the task of re-ranking.
-    Given a query and a list of documents, it computes the score [query, doc_i] for all possible
-    documents and sorts them in decreasing order. Then, MRR@10 and MAP is compute to measure the quality of the ranking.
-    :param samples: Must be a list and each element is of the form:
-        - {'query': '', 'positive': [], 'negative': []}. Query is the search query, positive is a list of positive
-        (relevant) documents, negative is a list of negative (irrelevant) documents.
-        - {'query': [], 'positive': [], 'negative': []}. Where query is a list of strings, which embeddings we average
-        to get the query embedding.
-    """
-
-    def compute_metrics_batched(self, model):
-        """
-        Computes the metrices in a batched way, by batching all queries and
-        all documents together
-        """
-
-        if hasattr(model, 'compute_score'):
-            return self.compute_metrics_batched_from_crossencoder(model)
-        else:
-            return self.compute_metrics_batched_from_biencoder(model)
-
-    def compute_metrics_batched_from_crossencoder(self, model):
-        all_mrr_scores = []
-        all_ap_scores = []
-
-        pairs = []
-        for sample in tqdm(self.samples, desc="Evaluating"):
-            for p in sample['positive']:
-                pairs.append([sample['query'], p])
-            for n in sample['negative']:
-                pairs.append([sample['query'], n])
-        all_scores = model.compute_score(pairs)
-        all_scores = np.array(all_scores)
-
-        start_inx = 0
-        for sample in tqdm(self.samples, desc="Evaluating"):
-            is_relevant = [True] * len(sample['positive']) + [False] * len(sample['negative'])
-            pred_scores = all_scores[start_inx:start_inx + len(is_relevant)]
-            start_inx += len(is_relevant)
-
-            pred_scores_argsort = np.argsort(-pred_scores)  # Sort in decreasing order
-            mrr = self.mrr_at_k_score(is_relevant, pred_scores_argsort, self.mrr_at_k)
-            ap = self.ap_score(is_relevant, pred_scores)
-
-            all_mrr_scores.append(mrr)
-            all_ap_scores.append(ap)
-
-        mean_ap = np.mean(all_ap_scores)
-        mean_mrr = np.mean(all_mrr_scores)
-
-        return {"map": mean_ap, "mrr": mean_mrr}
-
-    def compute_metrics_batched_from_biencoder(self, model):
-        all_mrr_scores = []
-        all_ap_scores = []
-        logger.info("Encoding queries...")
-        if isinstance(self.samples[0]["query"], str):
-            if hasattr(model, 'encode_queries'):
-                all_query_embs = model.encode_queries(
-                    [sample["query"] for sample in self.samples],
-                    convert_to_tensor=True,
-                    batch_size=self.batch_size,
-                )
-            else:
-                all_query_embs = model.encode(
-                    [sample["query"] for sample in self.samples],
-                    convert_to_tensor=True,
-                    batch_size=self.batch_size,
-                )
-        elif isinstance(self.samples[0]["query"], list):
-            # In case the query is a list of strings, we get the most similar embedding to any of the queries
-            all_query_flattened = [q for sample in self.samples for q in sample["query"]]
-            if hasattr(model, 'encode_queries'):
-                all_query_embs = model.encode_queries(all_query_flattened, convert_to_tensor=True,
-                                                      batch_size=self.batch_size)
-            else:
-                all_query_embs = model.encode(all_query_flattened, convert_to_tensor=True, batch_size=self.batch_size)
-        else:
-            raise ValueError(f"Query must be a string or a list of strings but is {type(self.samples[0]['query'])}")
-
-        logger.info("Encoding candidates...")
-        all_docs = []
-        for sample in self.samples:
-            all_docs.extend(sample["positive"])
-            all_docs.extend(sample["negative"])
-
-        all_docs_embs = model.encode(all_docs, convert_to_tensor=True, batch_size=self.batch_size)
-
-        # Compute scores
-        logger.info("Evaluating...")
-        query_idx, docs_idx = 0, 0
-        for instance in self.samples:
-            num_subqueries = len(instance["query"]) if isinstance(instance["query"], list) else 1
-            query_emb = all_query_embs[query_idx: query_idx + num_subqueries]
-            query_idx += num_subqueries
-
-            num_pos = len(instance["positive"])
-            num_neg = len(instance["negative"])
-            docs_emb = all_docs_embs[docs_idx: docs_idx + num_pos + num_neg]
-            docs_idx += num_pos + num_neg
-
-            if num_pos == 0 or num_neg == 0:
-                continue
-
-            is_relevant = [True] * num_pos + [False] * num_neg
-
-            scores = self._compute_metrics_instance(query_emb, docs_emb, is_relevant)
-            all_mrr_scores.append(scores["mrr"])
-            all_ap_scores.append(scores["ap"])
-
-        mean_ap = np.mean(all_ap_scores)
-        mean_mrr = np.mean(all_mrr_scores)
-
-        return {"map": mean_ap, "mrr": mean_mrr}
-
-
-def evaluate(self, model, split="test", **kwargs):
-    if not self.data_loaded:
-        self.load_data()
-
-    data_split = self.dataset[split]
-
-    evaluator = ChineseRerankingEvaluator(data_split, **kwargs)
-    scores = evaluator(model)
-
-    return dict(scores)
-
-
-AbsTaskReranking.evaluate = evaluate
+from mteb.abstasks.AbsTaskReranking import AbsTaskReranking
+from mteb.abstasks.TaskMetadata import TaskMetadata
 
 
 class T2Reranking(AbsTaskReranking):
-    @property
-    def description(self):
-        return {
-            'name': 'T2Reranking',
-            'hf_hub_name': "C-MTEB/T2Reranking",
-            'description': 'T2Ranking: A large-scale Chinese Benchmark for Passage Ranking',
-            "reference": "https://arxiv.org/abs/2304.03679",
-            'type': 'Reranking',
-            'category': 's2p',
-            'eval_splits': ['dev'],
-            'eval_langs': ['zh'],
-            'main_score': 'map',
-        }
-
-
-class T2RerankingZh2En(AbsTaskReranking):
-    @property
-    def description(self):
-        return {
-            'name': 'T2RerankingZh2En',
-            'hf_hub_name': "C-MTEB/T2Reranking_zh2en",
-            'description': 'T2Ranking: A large-scale Chinese Benchmark for Passage Ranking',
-            "reference": "https://arxiv.org/abs/2304.03679",
-            'type': 'Reranking',
-            'category': 's2p',
-            'eval_splits': ['dev'],
-            'eval_langs': ['zh2en'],
-            'main_score': 'map',
-        }
-
-
-class T2RerankingEn2Zh(AbsTaskReranking):
-    @property
-    def description(self):
-        return {
-            'name': 'T2RerankingEn2Zh',
-            'hf_hub_name': "C-MTEB/T2Reranking_en2zh",
-            'description': 'T2Ranking: A large-scale Chinese Benchmark for Passage Ranking',
-            "reference": "https://arxiv.org/abs/2304.03679",
-            'type': 'Reranking',
-            'category': 's2p',
-            'eval_splits': ['dev'],
-            'eval_langs': ['en2zh'],
-            'main_score': 'map',
-        }
+    metadata = TaskMetadata(
+        name="T2Reranking",
+        description="T2Ranking: A large-scale Chinese Benchmark for Passage Ranking",
+        reference="https://arxiv.org/abs/2304.03679",
+        dataset={
+            "path": "C-MTEB/T2Reranking",
+            "revision": "76631901a18387f85eaa53e5450019b87ad58ef9",
+        },
+        type="Reranking",
+        category="s2s",
+        modalities=["text"],
+        eval_splits=["dev"],
+        eval_langs=["cmn-Hans"],
+        main_score="map",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        annotations_creators=None,
+        dialect=None,
+        sample_creation=None,
+        bibtex_citation="""@misc{xie2023t2ranking,
+      title={T2Ranking: A large-scale Chinese Benchmark for Passage Ranking}, 
+      author={Xiaohui Xie and Qian Dong and Bingning Wang and Feiyang Lv and Ting Yao and Weinan Gan and Zhijing Wu and Xiangsheng Li and Haitao Li and Yiqun Liu and Jin Ma},
+      year={2023},
+      eprint={2304.03679},
+      archivePrefix={arXiv},
+      primaryClass={cs.IR}
+}""",
+        descriptive_stats={"n_samples": None, "avg_character_length": None},
+    )
 
 
 class MMarcoReranking(AbsTaskReranking):
-    @property
-    def description(self):
-        return {
-            'name': 'MMarcoReranking',
-            'hf_hub_name': "C-MTEB/Mmarco-reranking",
-            'description': 'mMARCO is a multilingual version of the MS MARCO passage ranking dataset',
-            "reference": "https://github.com/unicamp-dl/mMARCO",
-            'type': 'Reranking',
-            'category': 's2p',
-            'eval_splits': ['dev'],
-            'eval_langs': ['zh'],
-            'main_score': 'map',
-        }
+    metadata = TaskMetadata(
+        name="MMarcoReranking",
+        description="mMARCO is a multilingual version of the MS MARCO passage ranking dataset",
+        reference="https://github.com/unicamp-dl/mMARCO",
+        dataset={
+            "path": "C-MTEB/Mmarco-reranking",
+            "revision": "8e0c766dbe9e16e1d221116a3f36795fbade07f6",
+        },
+        type="Reranking",
+        category="s2s",
+        modalities=["text"],
+        eval_splits=["dev"],
+        eval_langs=["cmn-Hans"],
+        main_score="map",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        annotations_creators=None,
+        dialect=None,
+        sample_creation=None,
+        bibtex_citation="""@misc{bonifacio2021mmarco,
+      title={mMARCO: A Multilingual Version of MS MARCO Passage Ranking Dataset}, 
+      author={Luiz Henrique Bonifacio and Vitor Jeronymo and Hugo Queiroz Abonizio and Israel Campiotti and Marzieh Fadaee and  and Roberto Lotufo and Rodrigo Nogueira},
+      year={2021},
+      eprint={2108.13897},
+      archivePrefix={arXiv},
+      primaryClass={cs.CL}
+}""",
+        descriptive_stats={"n_samples": None, "avg_character_length": None},
+    )
 
 
 class CMedQAv1(AbsTaskReranking):
-    @property
-    def description(self):
-        return {
-            'name': 'CMedQAv1',
-            "hf_hub_name": "C-MTEB/CMedQAv1-reranking",
-            'description': 'Chinese community medical question answering',
-            "reference": "https://github.com/zhangsheng93/cMedQA",
-            'type': 'Reranking',
-            'category': 's2p',
-            'eval_splits': ['test'],
-            'eval_langs': ['zh'],
-            'main_score': 'map',
-        }
+    metadata = TaskMetadata(
+        name="CMedQAv1-reranking",
+        description="Chinese community medical question answering",
+        reference="https://github.com/zhangsheng93/cMedQA",
+        dataset={
+            "path": "C-MTEB/CMedQAv1-reranking",
+            "revision": "8d7f1e942507dac42dc58017c1a001c3717da7df",
+        },
+        type="Reranking",
+        category="s2s",
+        modalities=["text"],
+        eval_splits=["test"],
+        eval_langs=["cmn-Hans"],
+        main_score="map",
+        date=("2017-01-01", "2017-07-26"),
+        domains=["Medical", "Written"],
+        task_subtypes=[],
+        license="not specified",
+        annotations_creators="expert-annotated",
+        dialect=[],
+        sample_creation="found",
+        bibtex_citation="""@article{zhang2017chinese,
+  title={Chinese Medical Question Answer Matching Using End-to-End Character-Level Multi-Scale CNNs},
+  author={Zhang, Sheng and Zhang, Xin and Wang, Hui and Cheng, Jiajun and Li, Pei and Ding, Zhaoyun},
+  journal={Applied Sciences},
+  volume={7},
+  number={8},
+  pages={767},
+  year={2017},
+  publisher={Multidisciplinary Digital Publishing Institute}
+}""",
+        descriptive_stats={
+            "n_samples": {"test": 2000},
+            "avg_character_length": {"test": 165},
+        },
+    )
 
 
 class CMedQAv2(AbsTaskReranking):
-    @property
-    def description(self):
-        return {
-            'name': 'CMedQAv2',
-            "hf_hub_name": "C-MTEB/CMedQAv2-reranking",
-            'description': 'Chinese community medical question answering',
-            "reference": "https://github.com/zhangsheng93/cMedQA2",
-            'type': 'Reranking',
-            'category': 's2p',
-            'eval_splits': ['test'],
-            'eval_langs': ['zh'],
-            'main_score': 'map',
-        }
+    metadata = TaskMetadata(
+        name="CMedQAv2-reranking",
+        description="Chinese community medical question answering",
+        reference="https://github.com/zhangsheng93/cMedQA2",
+        dataset={
+            "path": "C-MTEB/CMedQAv2-reranking",
+            "revision": "23d186750531a14a0357ca22cd92d712fd512ea0",
+        },
+        type="Reranking",
+        category="s2s",
+        modalities=["text"],
+        eval_splits=["test"],
+        eval_langs=["cmn-Hans"],
+        main_score="map",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        annotations_creators=None,
+        dialect=None,
+        sample_creation=None,
+        bibtex_citation="""@ARTICLE{8548603, 
+author={S. Zhang and X. Zhang and H. Wang and L. Guo and S. Liu}, 
+journal={IEEE Access}, 
+title={Multi-Scale Attentive Interaction Networks for Chinese Medical Question Answer Selection}, 
+year={2018}, 
+volume={6}, 
+number={}, 
+pages={74061-74071}, 
+keywords={Biomedical imaging;Data mining;Semantics;Medical services;Feature extraction;Knowledge discovery;Medical question answering;interactive attention;deep learning;deep neural networks}, 
+doi={10.1109/ACCESS.2018.2883637}, 
+ISSN={2169-3536}, 
+month={},}""",
+        descriptive_stats={"n_samples": None, "avg_character_length": None},
+    )
