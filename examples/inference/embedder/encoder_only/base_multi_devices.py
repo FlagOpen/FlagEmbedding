5fd10485488dcc47af889a2446caed1884178865
hanhainebula <2512674094@qq.com>
2024-10-19 10:14:19 +0800

    upload singe & multi device examples for embedder

---
+++ examples/inference/embedder/encoder_only/base_multi_devices.py
@@ -0,0 +1,34 @@
+import os
+from FlagEmbedding import FlagModel
+
+
+def test_base_multi_devices():
+    model = FlagModel(
+        'BAAI/bge-small-en-v1.5',
+        normalize_embeddings=True,
+        use_fp16=True,
+        query_instruction_for_retrieval="Represent this sentence for searching relevant passages: ",
+        query_instruction_format="{}{}",
+        devices=["cuda:0", "cuda:1"],   # if you don't have GPUs, you can use ["cpu", "cpu"]
+        pooling_method='cls',
+        cache_dir=os.getenv('HF_HOME', None),
+    )
+    
+    queries = [
+        "What is the capital of France?",
+        "What is the population of China?",
+    ] * 100
+    passages = [
+        "Paris is the capital of France.",
+        "The population of China is over 1.4 billion people."
+    ] * 100
+    
+    queries_embeddings = model.encode_queries(queries)
+    passages_embeddings = model.encode_corpus(passages)
+    
+    cos_scores = queries_embeddings @ passages_embeddings.T
+    print(cos_scores[:2, :2])
+
+
+if __name__ == '__main__':
+    test_base_multi_devices()
--- examples/inference/embedder/encoder_only/base_singe_device.py
+++ examples/inference/embedder/encoder_only/base_singe_device.py
@@ -2,13 +2,14 @@ import os
 from FlagEmbedding import FlagModel
 
 
-def test_base():
+def test_base_single_device():
     model = FlagModel(
         'BAAI/bge-small-en-v1.5',
         normalize_embeddings=True,
         use_fp16=True,
         query_instruction_for_retrieval="Represent this sentence for searching relevant passages: ",
         query_instruction_format="{}{}",
+        devices="cuda:0",   # if you don't have a GPU, you can use "cpu"
         pooling_method='cls',
         cache_dir=os.getenv('HF_HOME', None),
     )
@@ -16,18 +17,18 @@ def test_base():
     queries = [
         "What is the capital of France?",
         "What is the population of China?",
-    ]
+    ] * 100
     passages = [
         "Paris is the capital of France.",
         "The population of China is over 1.4 billion people."
-    ]
+    ] * 100
     
     queries_embeddings = model.encode_queries(queries)
     passages_embeddings = model.encode_corpus(passages)
     
     cos_scores = queries_embeddings @ passages_embeddings.T
-    print(cos_scores)
+    print(cos_scores[:2, :2])
 
 
 if __name__ == '__main__':
-    test_base()
+    test_base_single_device()
