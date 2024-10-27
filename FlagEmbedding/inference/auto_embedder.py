04d0468f92296a91b111ff4a13c2e518fda38d9b
hanhainebula <2512674094@qq.com>
2024-10-27 21:48:12 +0800

    update auto embedder & reranker API

--- FlagEmbedding/inference/auto_embedder.py
+++ FlagEmbedding/inference/auto_embedder.py
@@ -1,7 +1,10 @@
 import os
-from typing import List, Union
+from typing import List, Union, Optional
 
-from FlagEmbedding.inference.embedder.model_mapping import MODEL_MAPPING
+from FlagEmbedding.inference.embedder.model_mapping import (
+    EmbedderModelClass,
+    AUTO_EMBEDDER_MAPPING, EMBEDDER_CLASS_MAPPING
+)
 
 
 class FlagAutoModel:
@@ -9,38 +12,48 @@ class FlagAutoModel:
         raise EnvironmentError(
             "FlagAutoModel is designed to be instantiated using the `FlagAutoModel.from_finetuned(model_name_or_path)` method."
         )
-    
+
     @classmethod
     def from_finetuned(
         cls,
         model_name_or_path: str,
+        model_class: Optional[Union[str, EmbedderModelClass]] = None,
         normalize_embeddings: bool = True,
         use_fp16: bool = True,
-        query_instruction_for_retrieval: str = None,
-        devices: Union[str, List[str]] = None,
+        query_instruction_for_retrieval: Optional[str] = None,
+        devices: Optional[Union[str, List[str]]] = None,
+        pooling_method: Optional[str] = None,
+        trust_remote_code: Optional[bool] = None,
+        query_instruction_format: Optional[str] = None,
         **kwargs,
     ):
         model_name = os.path.basename(model_name_or_path)
         if model_name.startswith("checkpoint-"):
             model_name = os.path.basename(os.path.dirname(model_name_or_path))
 
-        if model_name not in MODEL_MAPPING:
-            raise ValueError(
-                f"Model name '{model_name}' not found in the model mapping. You can pull request to add the model to "
-                "`https://github.com/FlagOpen/FlagEmbedding/tree/master/FlagEmbedding/inference/embedder/model_mapping.py`. " 
-                "If need, you can create a new `<model>.py` file in `https://github.com/FlagOpen/FlagEmbedding/tree/master/FlagEmbedding/inference/embedder/encoder_only` "
-                "or `https://github.com/FlagOpen/FlagEmbedding/tree/master/FlagEmbedding/inference/embedder/decoder_only`. "
-                "Welcome to contribute! You can also directly use the corresponding model class to instantiate the model."
-            )
+        if model_class is not None:
+            _model_class = EMBEDDER_CLASS_MAPPING[EmbedderModelClass(model_class)]
+        else:
+            if model_name not in AUTO_EMBEDDER_MAPPING:
+                raise ValueError(
+                    f"Model name '{model_name}' not found in the model mapping. You can pull request to add the model to "
+                    "`https://github.com/FlagOpen/FlagEmbedding/tree/master/FlagEmbedding/inference/embedder/model_mapping.py`. " 
+                    "If need, you can create a new `<model>.py` file in `https://github.com/FlagOpen/FlagEmbedding/tree/master/FlagEmbedding/inference/embedder/encoder_only` "
+                    "or `https://github.com/FlagOpen/FlagEmbedding/tree/master/FlagEmbedding/inference/embedder/decoder_only`. "
+                    "Welcome to contribute! You can also directly specify the corresponding `model_class` to instantiate the model."
+                )
 
-        model_config = MODEL_MAPPING[model_name]
+            model_config = AUTO_EMBEDDER_MAPPING[model_name]
 
-        model_class = model_config.model_class
-        pooling_method = kwargs.pop("pooling_method", model_config.pooling_method.value)
-        trust_remote_code = kwargs.pop("trust_remote_code", model_config.trust_remote_code)
-        query_instruction_format = kwargs.pop("query_instruction_format", model_config.query_instruction_format)
+            _model_class = model_config.model_class
+            if pooling_method is None:
+                pooling_method = model_config.pooling_method.value
+            if trust_remote_code is None:
+                trust_remote_code = model_config.trust_remote_code
+            if query_instruction_format is None:
+                query_instruction_format = model_config.query_instruction_format
 
-        return model_class(
+        return _model_class(
             model_name_or_path,
             normalize_embeddings=normalize_embeddings,
             use_fp16=use_fp16,
