59545668a7015ba0387ccdcda65b0795b7c49e97
cfli <545999961@qq.com>
2024-10-30 20:26:40 +0800

    update msmarco eval

--- FlagEmbedding/evaluation/msmarco/__main__.py
+++ FlagEmbedding/evaluation/msmarco/__main__.py
@@ -5,19 +5,22 @@ from FlagEmbedding.evaluation.msmarco import (
     MSMARCOEvalRunner
 )
 
+def main():
+    parser = HfArgumentParser((
+        MSMARCOEvalArgs,
+        MSMARCOEvalModelArgs
+    ))
 
-parser = HfArgumentParser((
-    MSMARCOEvalArgs,
-    MSMARCOEvalModelArgs
-))
+    eval_args, model_args = parser.parse_args_into_dataclasses()
+    eval_args: MSMARCOEvalArgs
+    model_args: MSMARCOEvalModelArgs
 
-eval_args, model_args = parser.parse_args_into_dataclasses()
-eval_args: MSMARCOEvalArgs
-model_args: MSMARCOEvalModelArgs
+    runner = MSMARCOEvalRunner(
+        eval_args=eval_args,
+        model_args=model_args
+    )
 
-runner = MSMARCOEvalRunner(
-    eval_args=eval_args,
-    model_args=model_args
-)
+    runner.run()
 
-runner.run()
+if __name__ == "__main__":
+    main()
