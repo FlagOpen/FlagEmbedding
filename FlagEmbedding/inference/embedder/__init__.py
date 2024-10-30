ccf5199e1e304297c17ab68c572426a2ff27770d
hanhainebula <2512674094@qq.com>
2024-10-30 18:40:49 +0800

    update model_class parameters in evaluation - delete 'auto' option - support import `EmbedderModelClass` and `RerankerModelClass`

--- FlagEmbedding/inference/embedder/__init__.py
+++ FlagEmbedding/inference/embedder/__init__.py
@@ -1,9 +1,11 @@
 from .encoder_only import FlagModel, BGEM3FlagModel
 from .decoder_only import FlagICLModel, FlagLLMModel
+from .model_mapping import EmbedderModelClass
 
 __all__ = [
     "FlagModel",
     "BGEM3FlagModel",
     "FlagICLModel",
     "FlagLLMModel",
+    "EmbedderModelClass",
 ]
