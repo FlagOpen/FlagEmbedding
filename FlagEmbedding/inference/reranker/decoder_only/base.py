d125f07a89d6d388d664f43e1e9ba323a22a128c
cfli <545999961@qq.com>
2024-11-05 21:43:25 +0800

    update reranker inference

--- FlagEmbedding/inference/reranker/decoder_only/base.py
+++ FlagEmbedding/inference/reranker/decoder_only/base.py
@@ -68,16 +68,28 @@ class DatasetForReranker(Dataset):
     def __getitem__(self, item):
         query_inputs = self.all_queries_inputs[item]
         passage_inputs = self.all_passages_inputs[item]
-        item = self.tokenizer.prepare_for_model(
-            [self.tokenizer.bos_token_id] + query_inputs['input_ids'],
-            self.sep_inputs + passage_inputs['input_ids'],
-            truncation='only_second',
-            max_length=self.encode_max_length,
-            padding=False,
-            return_attention_mask=False,
-            return_token_type_ids=False,
-            add_special_tokens=False
-        )
+        if self.tokenizer.bos_token_id is not None and self.tokenizer.bos_token_id != self.tokenizer.pad_token_id:
+            item = self.tokenizer.prepare_for_model(
+                [self.tokenizer.bos_token_id] + query_inputs['input_ids'],
+                self.sep_inputs + passage_inputs['input_ids'],
+                truncation='only_second',
+                max_length=self.encode_max_length,
+                padding=False,
+                return_attention_mask=False,
+                return_token_type_ids=False,
+                add_special_tokens=False
+            )
+        else:
+            item = self.tokenizer.prepare_for_model(
+                query_inputs['input_ids'],
+                self.sep_inputs + passage_inputs['input_ids'],
+                truncation='only_second',
+                max_length=self.encode_max_length,
+                padding=False,
+                return_attention_mask=False,
+                return_token_type_ids=False,
+                add_special_tokens=False
+            )
         item['input_ids'] = item['input_ids'] + self.sep_inputs + self.prompt_inputs
         item['attention_mask'] = [1] * len(item['input_ids'])
         item.pop('token_type_ids') if 'token_type_ids' in item.keys() else None
@@ -289,16 +301,28 @@ class BaseLLMReranker(AbsReranker):
                     all_queries_inputs_sorted[:min(len(all_queries_inputs_sorted), batch_size)], 
                     all_passages_inputs_sorted[:min(len(all_passages_inputs_sorted), batch_size)]
                 ):
-                    item = self.tokenizer.prepare_for_model(
-                        [self.tokenizer.bos_token_id] + query_inputs['input_ids'],
-                        sep_inputs + passage_inputs['input_ids'],
-                        truncation='only_second',
-                        max_length=encode_max_length,
-                        padding=False,
-                        return_attention_mask=False,
-                        return_token_type_ids=False,
-                        add_special_tokens=False
-                    )
+                    if self.tokenizer.bos_token_id is not None and self.tokenizer.bos_token_id != self.tokenizer.pad_token_id:
+                        item = self.tokenizer.prepare_for_model(
+                            [self.tokenizer.bos_token_id] + query_inputs['input_ids'],
+                            sep_inputs + passage_inputs['input_ids'],
+                            truncation='only_second',
+                            max_length=encode_max_length,
+                            padding=False,
+                            return_attention_mask=False,
+                            return_token_type_ids=False,
+                            add_special_tokens=False
+                        )
+                    else:
+                        item = self.tokenizer.prepare_for_model(
+                            query_inputs['input_ids'],
+                            sep_inputs + passage_inputs['input_ids'],
+                            truncation='only_second',
+                            max_length=encode_max_length,
+                            padding=False,
+                            return_attention_mask=False,
+                            return_token_type_ids=False,
+                            add_special_tokens=False
+                        )
                     item['input_ids'] = item['input_ids'] + sep_inputs + prompt_inputs
                     item['attention_mask'] = [1] * len(item['input_ids'])
                     item.pop('token_type_ids') if 'token_type_ids' in item.keys() else None
@@ -358,16 +382,28 @@ class BaseLLMReranker(AbsReranker):
 
                 batch_inputs = []
                 for query_inputs, passage_inputs in zip(queries_inputs, passages_inputs):
-                    item = self.tokenizer.prepare_for_model(
-                        [self.tokenizer.bos_token_id] + query_inputs['input_ids'],
-                        sep_inputs + passage_inputs['input_ids'],
-                        truncation='only_second',
-                        max_length=encode_max_length,
-                        padding=False,
-                        return_attention_mask=False,
-                        return_token_type_ids=False,
-                        add_special_tokens=False
-                    )
+                    if self.tokenizer.bos_token_id is not None and self.tokenizer.bos_token_id != self.tokenizer.pad_token_id:
+                        item = self.tokenizer.prepare_for_model(
+                            [self.tokenizer.bos_token_id] + query_inputs['input_ids'],
+                            sep_inputs + passage_inputs['input_ids'],
+                            truncation='only_second',
+                            max_length=encode_max_length,
+                            padding=False,
+                            return_attention_mask=False,
+                            return_token_type_ids=False,
+                            add_special_tokens=False
+                        )
+                    else:
+                        item = self.tokenizer.prepare_for_model(
+                            query_inputs['input_ids'],
+                            sep_inputs + passage_inputs['input_ids'],
+                            truncation='only_second',
+                            max_length=encode_max_length,
+                            padding=False,
+                            return_attention_mask=False,
+                            return_token_type_ids=False,
+                            add_special_tokens=False
+                        )
                     item['input_ids'] = item['input_ids'] + sep_inputs + prompt_inputs
                     item['attention_mask'] = [1] * len(item['input_ids'])
                     item.pop('token_type_ids') if 'token_type_ids' in item.keys() else None
