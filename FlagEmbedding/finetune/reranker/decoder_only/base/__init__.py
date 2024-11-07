d50112c38ee58fe4de262168919d348c1d9b4e9f
ZiyiXia <joeyxia0805@163.com>
2024-11-07 14:32:24 +0000

    package dependency

--- FlagEmbedding/finetune/reranker/decoder_only/base/__init__.py
+++ FlagEmbedding/finetune/reranker/decoder_only/base/__init__.py
@@ -1,7 +1,10 @@
 from .modeling import CrossDecoderModel
 from .runner import DecoderOnlyRerankerRunner
+from .arguments import RerankerModelArguments
+from .trainer import DecoderOnlyRerankerTrainer
 
 __all__ = [
     "CrossDecoderModel",
-    "DecoderOnlyRerankerRunner"
+    "DecoderOnlyRerankerRunner",
+    "DecoderOnlyRerankerTrainer"
 ]
