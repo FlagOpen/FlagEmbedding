d2c4a681c6462f977c18e292bab13143610a3357
cfli <545999961@qq.com>
2024-11-08 15:32:54 +0800

    update distill

--- FlagEmbedding/abc/finetune/embedder/AbsDataset.py
+++ FlagEmbedding/abc/finetune/embedder/AbsDataset.py
@@ -130,7 +130,7 @@ class AbsEmbedderTrainDataset(Dataset):
             passages.append(data['neg'][neg_idx])
 
         if self.args.knowledge_distillation:
-            assert isinstance(data['pos_scores'], list) and isinstance(data['neg_scores', list])
+            assert isinstance(data['pos_scores'], list) and isinstance(data['neg_scores'], list)
             teacher_scores.append(data['pos_scores'][pos_idx])
             for neg_idx in neg_idxs:
                 teacher_scores.append(data['neg_scores'][neg_idx])
