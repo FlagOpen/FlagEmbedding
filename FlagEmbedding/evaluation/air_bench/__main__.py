974ce9a5c28be7cc65760a818657a8a6368905d5
hanhainebula <2512674094@qq.com>
2024-11-15 12:32:32 +0800

    update __main__.py for evaluation to fix bugs - avoid AttributeError: 'NoneType' object has no attribute 'SIGTERM'

--- FlagEmbedding/evaluation/air_bench/__main__.py
+++ FlagEmbedding/evaluation/air_bench/__main__.py
@@ -6,23 +6,27 @@ from FlagEmbedding.evaluation.air_bench import (
 )
 
 
-parser = HfArgumentParser((
-    AIRBenchEvalArgs,
-    AIRBenchEvalModelArgs
-))
+def main():
+    parser = HfArgumentParser((
+        AIRBenchEvalArgs,
+        AIRBenchEvalModelArgs
+    ))
 
-eval_args, model_args = parser.parse_args_into_dataclasses()
-eval_args: AIRBenchEvalArgs
-model_args: AIRBenchEvalModelArgs
+    eval_args, model_args = parser.parse_args_into_dataclasses()
+    eval_args: AIRBenchEvalArgs
+    model_args: AIRBenchEvalModelArgs
 
-runner = AIRBenchEvalRunner(
-    eval_args=eval_args,
-    model_args=model_args
-)
+    runner = AIRBenchEvalRunner(
+        eval_args=eval_args,
+        model_args=model_args
+    )
 
-runner.run()
+    runner.run()
 
-print("==============================================")
-print("Search results have been generated.")
-print("For computing metrics, please refer to the official AIR-Bench docs:")
-print("- https://github.com/AIR-Bench/AIR-Bench/blob/main/docs/submit_to_leaderboard.md")
+
+if __name__ == "__main__":
+    main()
+    print("==============================================")
+    print("Search results have been generated.")
+    print("For computing metrics, please refer to the official AIR-Bench docs:")
+    print("- https://github.com/AIR-Bench/AIR-Bench/blob/main/docs/submit_to_leaderboard.md")
--- FlagEmbedding/evaluation/custom/__main__.py
+++ FlagEmbedding/evaluation/custom/__main__.py
@@ -6,18 +6,23 @@ from FlagEmbedding.evaluation.custom import (
 )
 
 
-parser = HfArgumentParser((
-    CustomEvalArgs,
-    CustomEvalModelArgs
-))
+def main():
+    parser = HfArgumentParser((
+        CustomEvalArgs,
+        CustomEvalModelArgs
+    ))
 
-eval_args, model_args = parser.parse_args_into_dataclasses()
-eval_args: CustomEvalArgs
-model_args: CustomEvalModelArgs
+    eval_args, model_args = parser.parse_args_into_dataclasses()
+    eval_args: CustomEvalArgs
+    model_args: CustomEvalModelArgs
 
-runner = CustomEvalRunner(
-    eval_args=eval_args,
-    model_args=model_args
-)
+    runner = CustomEvalRunner(
+        eval_args=eval_args,
+        model_args=model_args
+    )
 
-runner.run()
+    runner.run()
+
+
+if __name__ == "__main__":
+    main()
