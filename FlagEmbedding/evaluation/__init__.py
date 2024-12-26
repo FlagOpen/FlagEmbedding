f7428196b1d1473feea45aa98c0f7bfe8e3c14fd
ZiyiXia <joeyxia0805@163.com>
2024-12-26 07:26:47 +0000

    update init dependency

---
+++ FlagEmbedding/evaluation/__init__.py
@@ -0,0 +1,7 @@
+from .air_bench import AIRBenchEvalModelArgs, AIRBenchEvalArgs, AIRBenchEvalRunner
+from .beir import *
+# from miracle import *
+# from mkqa import *
+# from mldr import *
+# from msmarco import *
+from mteb import *
