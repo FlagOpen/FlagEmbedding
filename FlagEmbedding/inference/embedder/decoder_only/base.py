c43a3c675945c455e8148b8885d4e2920da7967b
Jianlyu Chen <2512674094@qq.com>
2025-04-10 16:55:15 +0800

    Update base.py

--- FlagEmbedding/inference/embedder/decoder_only/base.py
+++ FlagEmbedding/inference/embedder/decoder_only/base.py
@@ -225,7 +225,7 @@ class BaseLLMEmbedder(AbsEmbedder):
         # tokenize without padding to get the correct length
         all_inputs = []
         for start_index in trange(0, len(sentences), batch_size, desc='pre tokenize',
-                                  disable=len(sentences) < 256):
+                                  disable=len(sentences) < batch_size):
             sentences_batch = sentences[start_index:start_index + batch_size]
             inputs_batch = self.tokenizer(
                 sentences_batch,
@@ -263,7 +263,7 @@ class BaseLLMEmbedder(AbsEmbedder):
         # encode
         all_embeddings = []
         for start_index in tqdm(range(0, len(sentences), batch_size), desc="Inference Embeddings",
-                                disable=len(sentences) < 256):
+                                disable=len(sentences) < batch_size):
             inputs_batch = all_inputs_sorted[start_index:start_index + batch_size]
             inputs_batch = self.tokenizer.pad(
                 inputs_batch,
